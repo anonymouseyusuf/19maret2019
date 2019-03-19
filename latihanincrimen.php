2c43b928a0152a2f7283287d7f71cb5199489973
anonymouseyusuf <yusufprogramer1@gmail.com>
2019-03-19 12:11:20 +0700

    belajar fungsi if dan for

---
+++ latihanincrimen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>incriment</title>
+</head>
+<body>
+	<?php
+	$y="yusuf";
+
+	for ($i=0; $i <= 10 ; $i++) { 
+		# code...
+
+		echo "Nomornya adalah : $i <br>";
+		echo $y++;
+	}
+	?>
+
+</body>
+</html>
---
+++ table.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>table</title>
+</head>
+<body>
+
+      <table>
+  <tr>
+    <th>Bulan</th>
+    <th>Tabungan</th>
+  </tr>
+
+  <?php for ($i=0; $i < 10; $i++) { ?>
+    <tr>
+        <td>Januari</td>
+       <td>Rp 100.000,-</td>
+    </tr>
+  <?php
+  }
+  ?>
+
+ 
+</table>
+
+
+</body>
+</html>
